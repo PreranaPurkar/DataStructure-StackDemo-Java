80719d661c2f9220468871ffeca172b8e2e0d1c7
PreranaPurkar <preranapurkar12@gmail.com>
2023-07-18 16:39:06 +0530

    [ADDED}:[Prerana]POP function used for delete the element from Stack

--- src/com/technospace/stackdemo/StackDemo.java
+++ src/com/technospace/stackdemo/StackDemo.java
@@ -7,17 +7,35 @@ public class StackDemo {
 	int[] stack = new int[5];
 	int top = -1;
 	Scanner scanner=new Scanner(System.in);
+	//Push function used for insert the element in stack
+	
 	public void push(){
 		if(top==stack.length-1)
 		{
 			System.err.println("Stack is Overflow");
 		}
 		else{
-			System.err.println("Enter the number to the stack");
+			System.err.println("Enter the element to the stack");
 			int n=scanner.nextInt();
 			top++;
 			stack[top]=n;
 		}
+	}
+	//Pop function used for delete the element from stack
+	public void pop(){
+		if(top==-1){
+			System.out.println("Stack is Underflow");
+		}
+		else{
+			System.out.println("Element POP ="+stack[top]);
+			top--;
+		}
+	}
+	//Display function used for Display the elements in stack
+	public void display(){
+		for(int i=top;i>=0;i--){
+			System.out.println(stack[i]);
+			}
 	}
 	public static void main(String[] args) {
 		StackDemo stackDemo = new StackDemo();
@@ -25,8 +43,10 @@ public class StackDemo {
 		stackDemo.push();
 		stackDemo.push();
 		stackDemo.push();
-		stackDemo.push();
-		stackDemo.push();
+		stackDemo.display();
+		stackDemo.pop();
+		stackDemo.display();
+		
 
 	}
 
