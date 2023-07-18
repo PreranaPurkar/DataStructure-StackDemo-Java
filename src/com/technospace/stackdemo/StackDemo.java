5c423e63c0abd1d6dba13bf698640789de4d0e36
PreranaPurkar <preranapurkar12@gmail.com>
2023-07-18 16:21:01 +0530

    [ADDED]:[Prerana] Used display function for display the element in Stack

--- src/com/technospace/stackdemo/StackDemo.java
+++ src/com/technospace/stackdemo/StackDemo.java
@@ -7,6 +7,8 @@ public class StackDemo {
 	int[] stack = new int[5];
 	int top = -1;
 	Scanner scanner=new Scanner(System.in);
+	//Push function used for insert the element in stack
+	
 	public void push(){
 		if(top==stack.length-1)
 		{
@@ -19,6 +21,12 @@ public class StackDemo {
 			stack[top]=n;
 		}
 	}
+	//Display function used for Display the elements in stack
+	public void display(){
+		for(int i=top;i>=0;i--){
+			System.out.println(stack[i]);
+		}
+	}
 	public static void main(String[] args) {
 		StackDemo stackDemo = new StackDemo();
 		stackDemo.push();
@@ -26,7 +34,7 @@ public class StackDemo {
 		stackDemo.push();
 		stackDemo.push();
 		stackDemo.push();
-		stackDemo.push();
+		stackDemo.display();
 
 	}
 
